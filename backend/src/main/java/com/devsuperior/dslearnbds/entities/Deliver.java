0027f0c44cf2440d54cfb70032e0b89ddaf4520d
DiegoVP66 <dvpdev33@gmail.com>
2021-12-13 23:01:29 -0300

    Preauthorizing methods

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Deliver.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Deliver.java
@@ -21,27 +21,24 @@ import com.devsuperior.dslearnbds.entities.enums.DeliverStatus;
 public class Deliver implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String uri;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	private DeliverStatus status;
 	private String feedback;
 	private Integer correctCount;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "lesson_id")
 	private Lesson lesson;
 
 	@ManyToOne
-	@JoinColumns({
-		@JoinColumn(name = "user_id"),
-		@JoinColumn(name = "offer_id")
-	})
+	@JoinColumns({ @JoinColumn(name = "user_id"), @JoinColumn(name = "offer_id") })
 	private Enrollment enrollment;
 
 	public Deliver() {
