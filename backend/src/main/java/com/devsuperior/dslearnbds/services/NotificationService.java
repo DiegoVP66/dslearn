20bc9320c32e66ff951914e033ad99aad65daae6
DiegoVP66 <dvpdev33@gmail.com>
2021-12-21 17:07:07 -0300

    Notifications Repository, service, resource

--- backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -21,10 +21,10 @@ public class NotificationService {
 	private AuthService authService;
 
 	@Transactional(readOnly = true)
-	public Page<NotificationDTO> notificationsForCurrentUser(Pageable pageable) {
+	public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pageable) {
 		User user = authService.authenticated();
 		
-		Page<Notification> list = notificationRepository.findByUser(user, pageable);
+		Page<Notification> list = notificationRepository.find(user, unreadOnly, pageable);
 		return list.map(x -> new NotificationDTO(x));
 	}
 
